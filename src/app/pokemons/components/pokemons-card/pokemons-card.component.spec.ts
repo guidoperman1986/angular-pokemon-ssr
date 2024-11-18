73d7a3f83558118ee08514b16000b4f103283452
gperman <guidoyaelperman@gmail.com>
2024-11-18 18:53:43 -0300

    pruebas pokemons card y pokemons list

--- src/app/pokemons/components/pokemons-card/pokemons-card.component.spec.ts
+++ src/app/pokemons/components/pokemons-card/pokemons-card.component.spec.ts
@@ -3,10 +3,18 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PokemonsCardComponent } from './pokemons-card.component';
 import { provideRouter } from '@angular/router';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { SimplePokemon } from '../../interfaces';
+import { By } from '@angular/platform-browser';
+
+const mockPokemon: SimplePokemon = {
+  id: '1',
+  name: 'bulbasaur',
+};
 
 describe('PokemonsCardComponent', () => {
-  let component: PokemonsCardComponent;
   let fixture: ComponentFixture<PokemonsCardComponent>;
+  let compiled: HTMLElement;
+  let component: PokemonsCardComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,12 +23,37 @@ describe('PokemonsCardComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(PokemonsCardComponent);
-    fixture.componentRef.setInput('pokemon', { id: '1', name: 'bulbasaur' });
+    compiled = fixture.nativeElement as HTMLElement;
+    fixture.componentRef.setInput('pokemon', mockPokemon);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the SimplePokemon signal inputValue', () => {
+    expect(component.pokemon()).toEqual(mockPokemon);
+  });
+
+  it('should render the pokemon name and image correctly', () => {
+    expect(component.pokemonImage()).toContain(mockPokemon.id);
+
+    const image = compiled.querySelector('img');
+    expect(image).toBeDefined();
+
+    const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${mockPokemon.id}.png`;
+    expect(image?.getAttribute('src')).toEqual(imageUrl);
+    expect(compiled.textContent).toContain(mockPokemon.name);
+  });
+
+  it('should have the propper ng-reflect-router-link', () => {
+    const divWithLink = compiled.querySelector('div');
+
+    expect(divWithLink?.getAttribute('ng-reflect-router-link')).toBeDefined();
+    expect(
+      divWithLink?.attributes.getNamedItem('ng-reflect-router-link')?.value
+    ).toEqual(`/pokemons,${mockPokemon.name}`);
   });
 });
--- src/app/pokemons/components/pokemons-list/pokemons-list.component.spec.ts
+++ src/app/pokemons/components/pokemons-list/pokemons-list.component.spec.ts
@@ -2,10 +2,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { PokemonsListComponent } from './pokemons-list.component';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { SimplePokemon } from '../../interfaces';
+
+const mockPokemon: SimplePokemon[] = [
+  {
+    id: '1',
+    name: 'bulbasaur',
+  },
+  {
+    id: '2',
+    name: 'ivysaur',
+  },
+];
 
 describe('PokemonsListComponent', () => {
   let component: PokemonsListComponent;
   let fixture: ComponentFixture<PokemonsListComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,12 +27,28 @@ describe('PokemonsListComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(PokemonsListComponent);
-    fixture.componentRef.setInput('pokemons', 'test');
+    fixture.componentRef.setInput('pokemons', mockPokemon);
+    compiled = fixture.nativeElement as HTMLElement;
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the list of pokemons with 2 pokemon cards', () => {
+    expect(compiled.querySelectorAll('app-pokemons-card').length).toBe(
+      mockPokemon.length
+    );
+  });
+
+  it('should render empty list of pokemons', () => {
+    fixture.componentRef.setInput('pokemons', []);
+    fixture.detectChanges();
+    expect(compiled.querySelectorAll('app-pokemons-card').length).toBe(0);
+    expect(compiled.querySelector('div')?.textContent).toContain(
+      'No hay pokémons'
+    );
   });
 });
